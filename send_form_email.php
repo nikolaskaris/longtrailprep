95714aec19d5de5e1e9cbffdc0e235b17c9fb830
Niko Laskaris <laskarisnik@gmail.com>
2018-09-12 10:16:02 -0400

    first commit

---
+++ send_form_email.php
@@ -0,0 +1,283 @@
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "nik@longtrailtutors.com";
+ 
+    $email_subject = "Subject: ".$_POST['category'];
+ 
+     
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['message'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+ 
+    $name = $_POST['name']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $message = $_POST['message']; // required
+
+
+
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$name)) {
+ 
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($message) < 2) {
+ 
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+
+    $email_message .= "Message: ".clean_string($message)."\n";
+
+
+ 
+     
+ 
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+ 
+ 
+ 
+<!-- include your own success html here -->
+ 
+ 
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Email Verification</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+  <meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+  <meta name="author" content="FREEHTML5.CO" />
+
+  <!-- 
+  //////////////////////////////////////////////////////
+
+  FREE HTML5 TEMPLATE 
+  DESIGNED & DEVELOPED by FREEHTML5.CO
+    
+  Website:    http://freehtml5.co/
+  Email:      info@freehtml5.co
+  Twitter:    http://twitter.com/fh5co
+  Facebook:     https://www.facebook.com/fh5co
+
+  //////////////////////////////////////////////////////
+   -->
+
+  <!-- Facebook and Twitter integration -->
+  <meta property="og:title" content=""/>
+  <meta property="og:image" content=""/>
+  <meta property="og:url" content=""/>
+  <meta property="og:site_name" content=""/>
+  <meta property="og:description" content=""/>
+  <meta name="twitter:title" content="" />
+  <meta name="twitter:image" content="" />
+  <meta name="twitter:url" content="" />
+  <meta name="twitter:card" content="" />
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+  <link rel="shortcut icon" href="images/favicon.ico">
+  
+  <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,700|Montserrat:400,700' rel='stylesheet' type='text/css'>
+  
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- Icomoon Icon Fonts-->
+  <link rel="stylesheet" href="css/icomoon.css">
+  <!-- Simple Line Icons -->
+  <link rel="stylesheet" href="css/simple-line-icons.css">
+  <!-- Owl Carousel -->
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <!-- Bootstrap  -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+
+  
+  <link rel="stylesheet" href="css/style.css">
+
+
+  <!-- Modernizr JS -->
+  <script src="js/modernizr-2.6.2.min.js"></script>
+  <!-- FEATHER ICONS -->
+  <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+  <!-- FOR IE9 below -->
+  <!--[if lt IE 9]>
+  <script src="js/respond.min.js"></script>
+  <![endif]-->
+
+  </head>
+  <body>
+  <header role="banner" id="fh5co-header">
+      <div class="container">
+        <!-- <div class="row"> -->
+          <nav class="navbar navbar-default">
+            <div class="navbar-header">
+              <!-- Mobile Toggle Menu Button -->
+          <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+                <a class="navbar-brand" href="index.html"> <img src="images/longtrail_logo.svg" alt="Long Trail Prep" height=""></a> 
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="index.html"><span style="color: #1BB08A">Home</span></a></li>
+                <li><a href="services.html"><span style="color: #1BB08A">Services</span></a></li>
+                <li><a href="our_tutors.html"><span style="color: #1BB08A">Our Tutors</span></a></li>
+                <li><a href="faq.html"><span style="color: #1BB08A">FAQ</span></a></li>
+                <li><a href="contact.html"><span style="color: #1BB08A">Contact</span></a></li>
+                
+              </ul>
+            </div>
+          </nav>
+        <!-- </div> -->
+      </div>
+  </header>
+
+  <div id="fh5co-our-services" data-section="services">
+    <div class="container" style="height: 80vh">
+      <div class="row row-bottom-padded-sm animate-box">
+        <div class="col-md-12 section-heading">
+          <div class="row" style="margin-left: 5vw; margin-right: 5vw;">
+            <h2>Thank you for your inquiry.</h2>
+            <br>
+            <p>We will be in touch shortly.</p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+  
+  <!-- <footer id="footer" role="contentinfo">
+    <div class="container">
+      <div class="row row-bottom-padded-sm">
+        <div class="col-md-12">
+          <p class="copyright text-center">&copy; 2018 Long Trail Educational Services. All Rights Reserved.
+           <br> Template by <a href="http://freehtml5.co/">freehtml5.co</a></p>
+        </div>
+      </div>
+    </div>
+  </footer> -->
+  
+  
+  <!-- jQuery -->
+  <script src="js/jquery.min.js"></script>
+  <!-- jQuery Easing -->
+  <script src="js/jquery.easing.1.3.js"></script>
+  <!-- Bootstrap -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- Waypoints -->
+  <script src="js/jquery.waypoints.min.js"></script>
+  <!-- Owl Carousel -->
+  <script src="js/owl.carousel.min.js"></script>
+  <!-- Main JS (Do not remove) -->
+  <script src="js/main.js"></script>
+
+  <!-- Feather replace -->
+  <script>
+      feather.replace()
+    </script>
+
+  </body>
+</html>
+ 
+ 
+ 
+<?php
+ 
+}
+ 
+?>
